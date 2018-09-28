416dc105470b86008b41d8bf75759e49dee96413
Victor Forsgren <forsgren.victor@gmail.com>
2018-09-28 22:10:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+// defaults to port 3000 and ./public/ folder
+const dir = process.env.DIR || "public"
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.static(dir))
+
+app.listen(port)
